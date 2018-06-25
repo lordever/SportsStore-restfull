70260edbe7012128cc2d972b2ba2127e70381d1b
alzh0817 <aleksandr.zheleznov@netcracker.com>
2018-06-25 18:10:39 +0400

    ProductRepository not ended page 145

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Product} from './product.model';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 @Injectable()
 export class StaticDataSource {
@@ -8,7 +9,7 @@ export class StaticDataSource {
     new Product(1, 'Product 1', 'Category 1', 'Product 1 (Category 1)', 100),
     new Product(2, 'Product 2', 'Category 1', 'Product 2 (Category 1)', 100),
     new Product(3, 'Product 3', 'Category 1', 'Product 3 (Category 1)', 100),
-    new Product(4, "Product 4", "Category 1", 'Product 4 (Category 1)', 100),
+    new Product(4, 'Product 4', "Category 1", 'Product 4 (Category 1)', 100),
     new Product(5, "Product 5", "Category 1", "Product 5 (Category 1)", 100),
     new Product(6, "Product 6", "Category 2", "Product 6 (Category 2)", 100),
     new Product(7, "Product 7", "Category 2", "Product 7 (Category 2)", 100),
