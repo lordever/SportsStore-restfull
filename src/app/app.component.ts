718ae89004979934659746b12cdbfe81bb2989d2
alzh0817 <aleksandr.zheleznov@netcracker.com>
2018-06-25 15:26:01 +0400

    Page 144

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app",
+  template: `<div class="bg-success p-a-1 text-xs-center">
+                This is Sports Store
+              </div> `,
 })
 export class AppComponent {
-  title = 'app';
 }
